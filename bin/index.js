ac3fc5179e9ae3f3a7017444a65146532b5222ec
linjiajun <linjiajun@youxin.com>
2020-03-13 20:59:11 +0800

    add inquire

--- bin/index.js
+++ bin/index.js
@@ -2,14 +2,27 @@
 const path = require("path");
 const fs = require("fs");
 const program = require("commander");
+const inquirer = require("inquirer");
 
-program
-    .version('1.0.0')
-    .usage('<keywords>')
-    .parse(process.argv);
+program.version("1.0.0").usage("<init>");
 
-if(!program.args.length) {
-    program.help();
-} else {
-    console.log('Keywords: ' + program.args);   
+program.arguments("<init> [env]").action(function(cmd, env) {
+  cmdValue = cmd;
+  envValue = env;
+  var questions = [
+    {
+      type: "input",
+      name: "first_name",
+      message: "What's your first name"
+    }
+  ];
+  inquirer.prompt(questions).then(answers => {
+    console.log(JSON.stringify(answers, null, "  "));
+  });
+});
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
 }
