a59fb89a9db66f52477bf25e73af66db4815c3b3
linjiajun <linjiajun@youxin.com>
2020-03-14 09:11:05 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 const program = require("commander");
 const inquirer = require("inquirer");
 
-program.version("1.0.0").usage("<init>");
-program.arguments("<init> [env]").action(function(cmd, env) {
+program.version("1.0.0").usage("<run>");
+program.arguments("<run> [env]").action(function(cmd, env) {
   cmdValue = cmd;
   envValue = env;
   var questions = [
